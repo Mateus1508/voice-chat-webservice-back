bcdc707fbb65b46740b722d80ca28022afdf15df
Mateus Belmonte <mateus.santos@suporterenova.onmicrosoft.com>
2024-07-05 19:58:41 -0300

    feat: configuração básica do projeto

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const dialogflow = require('@google-cloud/dialogflow');
+const fs = require('fs');
+const path = require('path');
+const { v4 } = require('uuid');
+
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+
+const sessionClient = new dialogflow.SessionsClient({
+    credentials: {
+        private_key: CREDENTIALS.private_key,
+        client_email: CREDENTIALS.client_email,
+    }
+});
+
+const projectId = CREDENTIALS.project_id;
+const sessionId = v4();
+const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+async function detectIntent(queryText, language) {
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: queryText,
+        languageCode: language,
+      },
+    },
+  };
+
+  const responses = await sessionClient.detectIntent(request);
+  const result = responses[0].queryResult;
+
+  console.log(`Query: ${result.queryText}`);
+  console.log(`Response: ${result.fulfillmentText}`);
+
+  return result;
+}
+
+detectIntent('Como vai você?', 'pt-br')
+  .then(result => console.log(result))
+  .catch(err => console.error('ERROR:', err));
